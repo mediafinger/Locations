b0ff9398f97a61ef341fd25ff23f82e44d9d2c31
Andreas Finger <andy@mediafinger.com>
2014-08-17 00:53:59 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#encoding: utf-8
+
+if Rails.env == "development"
+ # puts "Cleaning DB first ..."
+ # Location.destroy_all
+end
+
+puts "Creating Locations"  # Always create seeds with lat & long & result to not cause API calls!
+Location.create!(latitude: 41.3830973, longitude: 2.1675154,    search: "Barcelona, Spain, Plaça dels Ángels, 1,", result: "Plaça Àngels, 1, 08001 Barcelona, Barcelona, Spain")
+Location.create!(latitude: 41.3788985, longitude: 2.1694266,    search: "Barcelona, Spain, Rambla del Raval", result: "Rambla del Raval, 25, 08001 Barcelona, Barcelona, Spain")
+Location.create!(latitude: 41.379405,  longitude: 2.180371,     city: "Barcelona", street: "Carrer de Merce, 5", result: "Carrer de la Mercè, 5, 08002 Barcelona, Barcelona, Spain")
+Location.create!(latitude: 41.2371851, longitude: 1.805886,     country: "ES", city: "Sitges", result: "Carrer d'Antoni Gaudí, 2-16, 08870 Sitges, Barcelona, Spain")
+Location.create!(latitude: 51.5073509, longitude: -0.1277583,   country: "UK", city: "London", result: "3 Whitehall, London SW1A 2EL, UK")
+Location.create!(latitude: 49.2568215, longitude: -123.1125015, result: "3200 Yukon Street, Vancouver, BC V5Y 3R9, Canada")
+Location.create!(latitude: 53.5511779, longitude: 9.964175899999999, search: "DE Hamburg 20359", result: "Rendsburger Straße 4, 20359 Hamburg, Germany")
